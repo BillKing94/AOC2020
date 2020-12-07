7306f575de497760460ddd5f0ed206c691515d83
Bill <bill@billking.io>
2020-12-06 20:33:02 -0800

    Added days 0-6

---
+++ Day07/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Day07
+{
+    class Program
+    {
+        static string[] SplitDoubleNewline(string input) => Regex.Split(input.Trim(), "\\n\\s*\\n");
+
+        static string[] SplitNewLine(string input) => input.Split('\n',
+            StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+        private static string input = @"
+
+";
+        
+        static void Main(string[] args)
+        {
+        }
+    }
+}
